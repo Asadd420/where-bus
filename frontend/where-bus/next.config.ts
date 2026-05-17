49b69faf469bf0062bec203d9331521baf785283
cha <t1110020m@gmail.com>
2026-05-17 13:55:33 +0800

    chore: preparation to deploy frontend server
    - setup environment variables
    - rewrite configuration in next.config.ts

--- frontend/where-bus/next.config.ts
+++ frontend/where-bus/next.config.ts
@@ -5,10 +5,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        // Whenever the frontend asks for /api/transit/...
-        source: '/api/transit/:path*',
-        // Silently proxy the request to the Spring Boot server
-        destination: 'http://localhost:8080/api/transit/:path*',
+        source: '/api/:path*',
+        destination: `${process.env.BACKEND_API_URL}/api/:path*`,
       },
     ]
   },
